7adf69789ed42178efc014810976b3017faa2958
PmgTeamDevelopment <djmk13brazil@gmail.com>
2025-12-18 19:21:40 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,5 @@ import fs from "fs";
 import path from "path";
 
 export default function Page() {
-  const htmlPath = path.join(process.cwd(), "public/hub/index.html");
-  const html = fs.readFileSync(htmlPath, "utf8");
-
-  return (
-    <html>
-      <head />
-      <body dangerouslySetInnerHTML={{ __html: html }} />
-    </html>
-  );
+  return null
 }
